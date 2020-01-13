3e64026d52530dca9eaab649e85023808861256d
STARTERGATE <32726809+startergate@users.noreply.github.com>
2020-01-13 09:45:54 +0900

    [CREATE] Base code

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 from slacker import Slacker
 from setting import token
+import asyncio
+import websockets
 
+print(token)
 slack = Slacker(token)
+
+response = slack.rtm.start()
+endpoint = response.body['url']
+
+
+async def main():
+    ws = await websockets.connect(endpoint)
+    while True:
+        message_json = await ws.recv()
+        print(message_json)
+        # slack.chat.post_message('#random', 'wow')
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_forever()
